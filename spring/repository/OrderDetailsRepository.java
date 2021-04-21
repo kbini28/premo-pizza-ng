3ffea1df117947eec324ac1c4b9842758dec2fa4
micelide <deanna.miceli@gmail.com>
2021-04-21 12:25:51 -0400

    Update OrderDetailsRepository.java

--- spring/repository/OrderDetailsRepository.java
+++ spring/repository/OrderDetailsRepository.java
@@ -1,9 +1,16 @@
 package org.premo.pizza.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import org.premo.pizza.model.OrderDetails;
+import org.premo.pizza.model.Product;
+import org.premo.pizza.model.Orders;
 
+@Repository
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
 
+	List<OrderDetails> findByProductAndOrder(Product product, Orders order);
 }
