4fe3549375d36c264bbc5c5127823240ed9e8e82
kbini <kbini@xpanxion.com>
2021-04-21 12:13:27 -0400

    Order service and add-order components Completed the service.ts file and add-order (html and ts) fixed the background image scaling wrong

--- src/app/orders/components/add-order/add-order.component.ts
+++ src/app/orders/components/add-order/add-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
   selector: 'app-add-order',
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-order.component.css']
 })
 export class AddOrderComponent implements OnInit {
+// calls the orderservice.create() method
 
-  constructor() { }
+  order = {
+    // should i add order and orderDetails models together?
+    custid: '',
+    empid: '',
+    dateplaced: '', // should this be dateplaced: Date?
+    discount: '',
+    quantity: '',
+    product: ''
+  }
+  submitted = false;
+
+  constructor(private ordersServce: OrdersService) { }
 
   ngOnInit(): void {
   }
 
+  // save
+  saveOrder(): void {
+    // set the data object for a new order
+    const data = {
+      custid: this.order.custid,
+      empid: this.order.empid,
+      dateplaced: this.order.dateplaced,
+      discount: this.order.discount,
+      quantity: this.order.quantity,
+      product: this.order.product
+    };
+
+    // create and subscribe to the action
+    this.ordersServce.create(data).subscribe(response => {
+      console.log(response);
+      this.submitted = true;
+    },
+    // handle errors
+    error => {
+      console.log(error);
+    });
+  } //end saveOrder()
+
+  // reset for a new order
+  newOrder(): void {
+    this.submitted = false;
+    this.order = {
+      custid: '',
+      empid: '',
+      dateplaced: '',
+      discount: '',
+      quantity: '',
+      product: '',
+    }
+  } // end newOrder()
 }
--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -1,9 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8080/api/orders';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
+// where you write all the standard CRUD operations
+// create the web service
+// NOTE: USE ordid FOR ALL INSTANCES OF THE ORDER ID or ID
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // returns all orders (get request)
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  // get an order by id (get request)
+  get(ordid): Observable<any> {
+    return this.http.get(`${baseUrl}/${ordid}`);
+  }
+
+  // create a new order (post request)
+  create(data): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  // update an order (get id, put request)
+  update(ordid, data): Observable<any> {
+    return this.http.put(`${baseUrl}/${ordid}`, data);
+  }
+
+  // delete an order (get id, delete request)
+  delete(ordid): Observable<any> {
+    return this.http.delete(`${baseUrl}/${ordid}`);
+  }
+
+  // skip the 'delete all' method
+
+  // MAKE SURE TO UPDATE WITH CORRECT SYNTAX, or REMOVE
+  // find by employee id??
+  findByEmployeeId(empid): Observable<any> {
+    return this.http.get(`${baseUrl}?empid=${empid}`);
+  }
+
+  // do we need findByDate() or findByCustomerId() ??
 }
