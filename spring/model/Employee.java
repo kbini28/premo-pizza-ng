d3b2a4ee747a4d4342186a57f9528660ac72005e
micelide <deanna.miceli@gmail.com>
2021-04-21 11:47:49 -0400

    Create Employee.java

---
+++ spring/model/Employee.java
@@ -0,0 +1,71 @@
+package org.premo.pizza.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "status")
+	private boolean status;
+	
+	public Employee() {}
+	public Employee(String name, String address, boolean status) {
+		this.name = name;
+		this.address = address;
+		this.status = status;
+	}
+	public Employee(long id, String name, String address, boolean status) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.status = status;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+}
