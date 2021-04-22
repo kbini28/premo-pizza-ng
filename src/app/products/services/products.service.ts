524709a63100eb922efec5d293d48d32069752f5
kbini <kbini@xpanxion.com>
2021-04-22 17:11:27 -0400

    update product url path in service update product listing in order details (wrong name)

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080/api/menu';
+const baseUrl = 'http://localhost:8080/api/products';
 
 @Injectable({
   providedIn: 'root'
